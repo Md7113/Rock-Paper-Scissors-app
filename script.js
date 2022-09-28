64e5b1b86bed0819a3b02403096ed189215b0d4a
Mason <davismasont@gmail.com>
2022-09-28 12:37:45 -0700

    everything else

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var tie = 0
 var rock = ('R')
 var paper = ('P')
 var scissors = ('S')
+var WLT = ('')
 function gamePlay() {
     for(start=1;start===1;){
     playerMove = prompt('Pick rock(R), papper(P), or Scissors(S)','')
@@ -18,49 +19,59 @@ function gamePlay() {
         if(playerMove === 'R'){
             if(compMove === 'R'){
                 tie = tie + 1
-                alert('It was a tie, Current records:: wins:'+win+' loses:'+lose+' ties:'+tie)
+                alert('It was a tie,')
             }
             if(compMove === 'P'){
                 lose = lose + 1
-                alert('you lost, Current records:: wins:'+win+' loses:'+lose+' ties:'+tie)
+                alert('you lost,')
             }
             if(compMove === 'S'){
                 win = win + 1
-                alert('you won, Current records:: wins:'+win+' loses:'+lose+' ties:'+tie)
+                alert('you won,')
             }
         }
         if(playerMove === 'P'){
             if(compMove === 'R'){
                 win = win + 1
-                alert('you won, Current records:: wins:'+win+' loses:'+lose+' ties:'+tie)
+                alert('you won,')
             }
             if(compMove === 'P'){
                 tie = tie + 1
-                alert('It was a tie, Current records:: wins:'+win+' loses:'+lose+' ties:'+tie)
+                alert('It was a tie,')
             }
             if(compMove === 'S'){
                 lose = lose + 1
-                alert('you lost, Current records:: wins:'+win+' loses:'+lose+' ties:'+tie)
+                alert('you lost,')
             }
         }
         if(playerMove === 'S'){
             if(compMove === 'R'){
                 lose = lose + 1
-                alert('you lost, Current records:: wins:'+win+' loses:'+lose+' ties:'+tie)
+                alert('you lost,')
             }
             if(compMove === 'P'){
                 win = win + 1
-                alert('you won, Current records:: wins:'+win+' loses:'+lose+' ties:'+tie)
+                alert('you won,')
             }
             if(compMove === 'S'){
                 tie = tie + 1
-                alert('It was a tie, Current records:: wins:'+win+' loses:'+lose+' ties:'+tie)
+                alert('It was a tie,')
             }
         }
 
     }else{
         alert('That was not an option please enter P, R, or S')
     }
+    WLT = 'Current Records:'
+    WLT += '\r\n'
+    WLT += 'wins:'+win
+    WLT += '\r\n'
+    WLT += 'Loses:'+lose
+    WLT += '\r\n'
+    WLT += 'ties:'+tie
+
+    alert(WLT)
+    
     contGame = confirm('Do you want to play again?')
     if(!contGame){
         start = 2
